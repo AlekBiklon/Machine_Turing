c1841cc1f0cfa3ee139d50610b879c3de6e9055a
AlekBiklon <sashabilokon82@gmail.com>
2023-10-07 19:33:44 +0300

    Update comments.

--- Machine_Turing.py
+++ Machine_Turing.py
@@ -1,10 +1,10 @@
-# Готова реалізація Машини Тьюринга, яка робить додавання.
-# Також тут представлений зразок циклу з постумовою ОРИГІНАЛ
+# Modeling a Turing machine that does addition.
+# A sample loop with a postcondition is also implemented here
 tape1=['|','|','|','*','|','|']
 print(tape1)
 
 while '*' in tape1:
-    tape1.append('') # цей вираз задовольняє умову нескінченності стрічки
+    tape1.append('') # this expression satisfies the condition that the tape is infinite
     for i in range(len(tape1)):
         if tape1[i]=='|':
             tape1[i]=''
